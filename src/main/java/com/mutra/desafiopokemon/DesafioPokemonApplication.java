63561c59f09471b3a2c379f431d55f7ae606c184
Mario Rabelo <mutrano@gmail.com>
2021-02-12 14:47:25 -0300

    Started project

---
+++ src/main/java/com/mutra/desafiopokemon/DesafioPokemonApplication.java
@@ -0,0 +1,13 @@
+package com.mutra.desafiopokemon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioPokemonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioPokemonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mutra/desafiopokemon/DesafioPokemonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mutra.desafiopokemon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioPokemonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
